5fc478dd0d1044a60e2dedd2390e04b735ba2762
Kunpeng-chen <76896982+Kunpeng-chen@users.noreply.github.com>
2024-02-21 11:10:19 +0800

    Update device.h

--- device.h
+++ device.h
@@ -9,24 +9,12 @@
 struct DeviceVtbl;
 // Device class 
 typedef struct {
-	struct DeviceVtbl const *vptr;
-	
 	//properties
 	uint8_t status;	
 } Device;
 
-// Device class virtual table
-struct DeviceVtbl {
-    void (*onEventFunc)(Device const * const me);
-};
-
 // Device class operation and interface
 void Device_ctor(Device * const me, uint8_t _status); 
 void Device_delete(Device * const me);
-
-static inline void Device_onEventFunc(Device const * const me) 
-{
-    (*me->vptr->onEventFunc)(me);
-}
 
 #endif /* DEVICE_H */
